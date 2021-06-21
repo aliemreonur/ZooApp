cccb31d181b49890ed1bd46ab5640010ba7005f6
Ali Emre Onur <aliemreonur@gmail.com>
2021-06-21 20:52:28 +0300

    Project Finished

---
+++ Assets/Scripts/CardView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardView : MonoBehaviour
+{
+    public Text title;
+    public Text description;
+    public Text exhibit;
+    public Image animalImg;
+
+    public CardModel[] cards;
+
+    public void callSelectedCard(int i)
+    {
+        title.text = cards[i].title;
+        description.text = cards[i].description;
+        exhibit.text = cards[i].exhibit;
+        animalImg.sprite = cards[i].animalImg;
+
+    }
+
+}
